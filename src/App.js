5672e706928fad0248c25b7b6228c874ea8b4d2c
ptai66 <88138894+ptai66@users.noreply.github.com>
2022-08-10 14:03:20 +0200

    adding photo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Photo from "./Photo.js"
+
+
+export default function App(){
+    return(
+    <>
+    <Photo/>
+    </>
+    )
+}
---
+++ src/Photo.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Photo(){
+    return <img src="../components/portrait.png" alt="Portrait"/>
+}
